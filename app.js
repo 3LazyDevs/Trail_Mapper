8afcdd5873aede8d3c19ddfb418bb8fe3db77708
3LazyDevs <116371521+3LazyDevs@users.noreply.github.com>
2023-06-01 15:11:50 +0530

    json in express

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT=3000;
 
 app.use(express.static("public"));
+app.use(express.json());
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + "/views/index.html");
